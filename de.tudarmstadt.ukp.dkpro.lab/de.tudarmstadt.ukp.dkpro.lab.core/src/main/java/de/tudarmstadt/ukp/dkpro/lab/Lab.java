5ca3599f64a9242527efdfd58950d6a25a18993b
REC <richard.eckart@gmail.com>
2011-10-30 14:56:06 +0100

    Removed unnecessary imports

--- de.tudarmstadt.ukp.dkpro.lab/de.tudarmstadt.ukp.dkpro.lab.core/src/main/java/de/tudarmstadt/ukp/dkpro/lab/Lab.java
+++ de.tudarmstadt.ukp.dkpro.lab/de.tudarmstadt.ukp.dkpro.lab.core/src/main/java/de/tudarmstadt/ukp/dkpro/lab/Lab.java
@@ -2,13 +2,13 @@
  * Copyright 2011
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- *   
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *   
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- *   
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,8 +19,6 @@ package de.tudarmstadt.ukp.dkpro.lab;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.logging.Handler;
-import java.util.logging.LogManager;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
