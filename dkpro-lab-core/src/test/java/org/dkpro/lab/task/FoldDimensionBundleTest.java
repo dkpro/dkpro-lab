3488262ddac0ec563c8d0d276a81613e1545b988
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-03-30 12:49:14 -0300

    No issue. Consistently use `\n` to generate strings in unit test to fix Windows build.

--- dkpro-lab-core/src/test/java/org/dkpro/lab/task/FoldDimensionBundleTest.java
+++ dkpro-lab-core/src/test/java/org/dkpro/lab/task/FoldDimensionBundleTest.java
@@ -42,95 +42,95 @@ import org.junit.rules.TestName;
 
 public class FoldDimensionBundleTest
 {
-	@Before
-	public void setup()
-	{
-		String path = "target/repository/"+getClass().getSimpleName()+"/"+name.getMethodName();
-		System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
-	}
+    @Before
+    public void setup()
+    {
+        String path = "target/repository/"+getClass().getSimpleName()+"/"+name.getMethodName();
+        System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
+    }
 
-	@Test
-	public void testSimpleFold()
-	{
-		Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
-				"8", "9", "10");
-		
-		FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
-		
-		String expected = 
-				"0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
-				"1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
-				"2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
+    @Test
+    public void testSimpleFold()
+    {
+        Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
+                "8", "9", "10");
+        
+        FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
+        
+        String expected = 
+                "0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
+                "1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
+                "2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
 
-		StringBuilder actual = new StringBuilder();
+        StringBuilder actual = new StringBuilder();
 
-		int n = 0;
-		ParameterSpace pSpace = new ParameterSpace(foldBundle);
-		for (Map<String, Object> config : pSpace) {
-			actual.append(String.format("%d - %s %s%n", n, config.get("fold_validation"),
-					config.get("fold_training")));
-			n++;
-		}
-		
-		assertEquals(3 , n);
-		assertEquals(3, pSpace.getStepCount());
-		assertEquals(expected, actual.toString());
-	}
+        int n = 0;
+        ParameterSpace pSpace = new ParameterSpace(foldBundle);
+        for (Map<String, Object> config : pSpace) {
+            actual.append(String.format("%d - %s %s\n", n, config.get("fold_validation"),
+                    config.get("fold_training")));
+            n++;
+        }
+        
+        assertEquals(3 , n);
+        assertEquals(3, pSpace.getStepCount());
+        assertEquals(expected, actual.toString());
+    }
 
-	@Test
-	public void testFileFold()
-	{
-		Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
-				"8", "9", "10");
-		
-		FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
-		
-		String expected = 
-				"0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
-				"1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
-				"2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
+    @Test
+    public void testFileFold()
+    {
+        Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
+                "8", "9", "10");
+        
+        FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
+        
+        String expected = 
+                "0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
+                "1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
+                "2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
 
-		StringBuilder actual = new StringBuilder();
+        StringBuilder actual = new StringBuilder();
 
-		int n = 0;
-		ParameterSpace pSpace = new ParameterSpace(foldBundle);
-		for (Map<String, Object> config : pSpace) {
-			actual.append(String.format("%d - %s %s%n", n, config.get("fold_validation"),
-					config.get("fold_training")));
-			n++;
-		}
-		
-		assertEquals(3 , n);
-		assertEquals(3, pSpace.getStepCount());
-		assertEquals(expected, actual.toString());
-	}
-	
-	/**
-	 * Tests that instances in the same cluster (here, parent folder) go into a fold together.
-	 */
+        int n = 0;
+        ParameterSpace pSpace = new ParameterSpace(foldBundle);
+        for (Map<String, Object> config : pSpace) {
+            actual.append(String.format("%d - %s %s\n", n, config.get("fold_validation"),
+                    config.get("fold_training")));
+            n++;
+        }
+        
+        assertEquals(3 , n);
+        assertEquals(3, pSpace.getStepCount());
+        assertEquals(expected, actual.toString());
+    }
+    
+    /**
+     * Tests that instances in the same cluster (here, parent folder) go into a fold together.
+     */
     @Test
     public void testComparator()
     {
             Dimension<String> baseData = Dimension.create("base", "aa/1.txt", "aa/2.txt", "aa/3.txt", 
-            				"bb/4.txt", "bb/5.txt", "bb/6.txt", "cc/7.txt",
-            				"cc/8.txt", "cc/9.txt", "cc/10.txt");
+                            "bb/4.txt", "bb/5.txt", "bb/6.txt", "cc/7.txt",
+                            "cc/8.txt", "cc/9.txt", "cc/10.txt");
             
             Comparator<String> comp = new Comparator<String>(){
 
-    			@Override
-    			public int compare(String filename1, String filename2)
-    			{
-    				File file1 = new File(filename1);
-    				File file2 = new File(filename2);
-    				String folder1 = file1.getParentFile().getName();
-    				String folder2 = file2.getParentFile().getName();
-    				
-            		if(folder1.equals(folder2)){
-            			return 0;
-            		}
-            		return 1;
-    			}
-        	};
+                @Override
+                public int compare(String filename1, String filename2)
+                {
+                    File file1 = new File(filename1);
+                    File file2 = new File(filename2);
+                    String folder1 = file1.getParentFile().getName();
+                    String folder2 = file2.getParentFile().getName();
+                    
+                    if(folder1.equals(folder2)){
+                        return 0;
+                    }
+                    return 1;
+                }
+            };
            
             FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3, comp);
            
@@ -145,7 +145,7 @@ public class FoldDimensionBundleTest
             int n = 0;
             ParameterSpace pSpace = new ParameterSpace(foldBundle);
             for (Map<String, Object> config : pSpace) {
-                    actual.append(String.format("%d - %s %s%n", n, config.get("fold_validation"),
+                    actual.append(String.format("%d - %s %s\n", n, config.get("fold_validation"),
                                     config.get("fold_training")));
                     n++;
             }
@@ -155,32 +155,32 @@ public class FoldDimensionBundleTest
             assertEquals(expected, actual.toString());
     }
     
-	/**
-	 * Tests to see that instances are evenly distributed across folds.
-	 */
+    /**
+     * Tests to see that instances are evenly distributed across folds.
+     */
     @Test
     public void testFoldDistribution()
     {
             Dimension<String> baseData = Dimension.create("base", "aa/1.txt", "aa/2.txt", "bb/3.txt", 
-            				"cc/4.txt", "dd/5.txt", "dd/6.txt", "ee/7.txt",
-            				"ff/8.txt", "gg/9.txt", "gg/10.txt");
+                            "cc/4.txt", "dd/5.txt", "dd/6.txt", "ee/7.txt",
+                            "ff/8.txt", "gg/9.txt", "gg/10.txt");
             
             Comparator<String> comp = new Comparator<String>(){
 
-    			@Override
-    			public int compare(String filename1, String filename2)
-    			{
-    				File file1 = new File(filename1);
-    				File file2 = new File(filename2);
-    				String folder1 = file1.getParentFile().getName();
-    				String folder2 = file2.getParentFile().getName();
-    				
-            		if(folder1.equals(folder2)){
-            			return 0;
-            		}
-            		return 1;
-    			}
-        	};
+                @Override
+                public int compare(String filename1, String filename2)
+                {
+                    File file1 = new File(filename1);
+                    File file2 = new File(filename2);
+                    String folder1 = file1.getParentFile().getName();
+                    String folder2 = file2.getParentFile().getName();
+                    
+                    if(folder1.equals(folder2)){
+                        return 0;
+                    }
+                    return 1;
+                }
+            };
            
             FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3, comp);
            
@@ -195,7 +195,7 @@ public class FoldDimensionBundleTest
             int n = 0;
             ParameterSpace pSpace = new ParameterSpace(foldBundle);
             for (Map<String, Object> config : pSpace) {
-                    actual.append(String.format("%d - %s %s%n", n, config.get("fold_validation"),
+                    actual.append(String.format("%d - %s %s\n", n, config.get("fold_validation"),
                                     config.get("fold_training")));
                     n++;
             }
@@ -205,66 +205,66 @@ public class FoldDimensionBundleTest
             assertEquals(3, pSpace.getStepCount());
             assertEquals(expected, actual.toString());
     }
-	
-	@Test
-	public void testFoldInjection() throws Exception
-	{
-		File repo = new File("target/repository/"+getClass().getSimpleName()+"/"+name.getMethodName());
-		FileUtils.deleteDirectory(repo);
-		repo.mkdirs();
-		((FileSystemStorageService) Lab.getInstance().getStorageService()).setStorageRoot(repo);
-		
-		Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
-				"8", "9", "10");
-		
-		FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
-		
-		String expected = 
-				"0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
-				"1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
-				"2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
+    
+    @Test
+    public void testFoldInjection() throws Exception
+    {
+        File repo = new File("target/repository/"+getClass().getSimpleName()+"/"+name.getMethodName());
+        FileUtils.deleteDirectory(repo);
+        repo.mkdirs();
+        ((FileSystemStorageService) Lab.getInstance().getStorageService()).setStorageRoot(repo);
+        
+        Dimension<String> baseData = Dimension.create("base", "1", "2", "3", "4", "5", "6", "7", 
+                "8", "9", "10");
+        
+        FoldDimensionBundle<String> foldBundle = new FoldDimensionBundle<String>("fold", baseData, 3);
+        
+        String expected = 
+                "0 - [1, 4, 7, 10] [2, 5, 8, 3, 6, 9]\n" +
+                "1 - [2, 5, 8] [1, 4, 7, 10, 3, 6, 9]\n" +
+                "2 - [3, 6, 9] [1, 4, 7, 10, 2, 5, 8]\n";
 
-		ParameterSpace pSpace = new ParameterSpace(foldBundle);
+        ParameterSpace pSpace = new ParameterSpace(foldBundle);
 
-		final StringBuilder actual = new StringBuilder();
+        final StringBuilder actual = new StringBuilder();
 
-		Task testTask = new ExecutableTaskBase()
-		{
-			int n = 0;
-			
-			@Discriminator
-			Collection<String> fold_validation;
-			
-			@Discriminator
-			Collection<String> fold_training;
-			
-			@Override
-			public void execute(TaskContext aContext)
-				throws Exception
-			{
-				System.out.printf("%d training  : %s%n", n, fold_training);
-				System.out.printf("%d validation: %s%n", n, fold_validation);
-				actual.append(String.format("%d - %s %s%n", n, fold_validation, fold_training));
-				n++;
-			}
-		};
-		
-		DefaultBatchTask batchTask = new DefaultBatchTask();
-		batchTask.setParameterSpace(pSpace);
-		batchTask.addTask(testTask);
-			
-		Lab.getInstance().run(batchTask);
-		
-		assertEquals(3, pSpace.getStepCount());
-		assertEquals(expected, actual.toString());
-	}
+        Task testTask = new ExecutableTaskBase()
+        {
+            int n = 0;
+            
+            @Discriminator
+            Collection<String> fold_validation;
+            
+            @Discriminator
+            Collection<String> fold_training;
+            
+            @Override
+            public void execute(TaskContext aContext)
+                throws Exception
+            {
+                System.out.printf("%d training  : %s\n", n, fold_training);
+                System.out.printf("%d validation: %s\n", n, fold_validation);
+                actual.append(String.format("%d - %s %s\n", n, fold_validation, fold_training));
+                n++;
+            }
+        };
+        
+        DefaultBatchTask batchTask = new DefaultBatchTask();
+        batchTask.setParameterSpace(pSpace);
+        batchTask.addTask(testTask);
+            
+        Lab.getInstance().run(batchTask);
+        
+        assertEquals(3, pSpace.getStepCount());
+        assertEquals(expected, actual.toString());
+    }
 
-	@Rule
-	public TestName name = new TestName();
+    @Rule
+    public TestName name = new TestName();
 
-	@Before
-	public void printSeparator()
-	{
-		System.out.println("\n=== " + name.getMethodName() + " =====================");
-	}
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
+    }
 }
